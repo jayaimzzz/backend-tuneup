85edf0c21edba9e340366d7ec2c3618a43dd1429
jayaimzzz <jameshorton@gmail.com>
2019-03-11 17:08:19 -0400

    completed part B

--- tuneup.py
+++ tuneup.py
@@ -11,7 +11,17 @@ import timeit
 
 def profile(func):
     """A function that can be used as a decorator to measure performance"""
-    raise NotImplementedError("Complete this decorator function")
+    def wrapper(*args, **kwargs):
+        print "before"
+        c = cProfile.Profile()
+        c.enable()
+        result = func(*args, **kwargs)
+        c.disable()
+        p = pstats.Stats(c)
+        p.sort_stats('cumulative').print_stats()
+        return result
+    return wrapper
+
 
 
 def read_movies(src):
@@ -28,7 +38,7 @@ def is_duplicate(title, movies):
             return True
     return False
 
-
+@profile
 def find_duplicate_movies(src):
     """Returns a list of duplicate movies from a src list"""
     movies = read_movies(src)
@@ -46,7 +56,7 @@ def timeit_helper():
     code = '''find_duplicate_movies("movies.txt")'''
     t = timeit.Timer(setup=imports, stmt=code)
     results = t.repeat(repeat=7,number=5)
-    print "Best time across 7 repeats of 5 runs per repeat: {}".format(min(results))
+    print "Best time across 7 repeats of 5 runs per repeat: {}".format(min(results) / 5)
 
 
 def main():
@@ -57,5 +67,5 @@ def main():
 
 
 if __name__ == '__main__':
-    # main()
-    timeit_helper()
+    main()
+    # timeit_helper()
